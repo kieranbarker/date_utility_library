12be5cad22af7042d19cc65cd5344714d7756610
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2022-01-29 17:27:24 +0000

    Update events demo

--- custom_events/app.js
+++ custom_events/app.js
@@ -1,15 +1,25 @@
 import Time from './time.js';
 
-const now = new Time();
-const tenYearsAway = now.addYears(10);
+// Create a new Time instance
+const halloween = new Time(undefined, 'October 31, 2021');
+console.log('Halloween', halloween);
 
+// Listen for time update events
 document.addEventListener('time:update', function (event) {
-  // Only allow dates in the next 10 years
-  if (event.detail.time.date >= tenYearsAway.date) {
+  // Update requested
+  console.log('Update requested', event.detail);
+
+  // If the year is greater than 2021, don't update
+  if (event.detail.time.date.getFullYear() > 2021) {
     event.preventDefault();
-    console.warn(`Only dates before ${tenYearsAway.date.toString()} are allowed.`);
+    console.warn('Updated canceled', event.detail);
+    return;
   }
+
+  // Update complete
+  console.log('Update complete', event.detail);
 });
 
-// This shows a warning
-now.addYears(10);
+// Adjust the date
+const yearNotUpdated = halloween.addDays(3).addMonths(1).addYears(1);
+console.warn('Year not updated', yearNotUpdated);
